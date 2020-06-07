c9e437b0919b97ccb5a475291e76712f0958d7fb
hyeonhee127 <hyeonhee127@gmail.com>
2020-06-07 23:58:06 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     siteTitle: `Home`,
     siteLanguage: `en`,
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    
   },
   plugins: [
     {
@@ -74,3 +75,44 @@ module.exports = {
     `gatsby-plugin-netlify`,
   ],
 }
+
+const Footer = () => {
+  const { siteTitle } = useSiteMetadata()
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by {Hyeonhee Park}. All rights reserved.
+      </div>
+      <div>
+        <Link
+          aria-label="Link to the theme's GitHub repository"
+          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog"
+        >
+          Theme
+        </Link>
+        {` `}
+        by
+        {` `}
+        <Link aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
+          Hyeonhee Park
+        </Link>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
